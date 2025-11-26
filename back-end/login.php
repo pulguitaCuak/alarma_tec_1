cff3df6435a0762d4bbe56c481a46bf051f38b21
Cangrejoide <alejoezegodoy2007@gmail.com>
2025-11-25 21:05:10 -0300

    Cambien la pagina que redirige el login.php 2

--- back-end/login.php
+++ back-end/login.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 $_SESSION["apellido"] = $user["apellido"];
                 $_SESSION["cargo"] = $user["id_cargo"];
 
-                header("Location: ../frontend/dashboardEquipos.php");
+                header("Location: ../frontend/dashboardEquipos.html");
                 exit;
             } else {
                 echo "<script>alert('Contraseña incorrecta.'); window.history.back();</script>";
