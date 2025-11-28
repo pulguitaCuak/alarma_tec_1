1ff3129eba50fbdc4168f93a7395a6a829dc82cc
Cangrejoide <alejoezegodoy2007@gmail.com>
2025-11-28 00:14:07 -0300

    Lo que llegue hacer antes

---
+++ frontend/js/dashboardSensor.js
@@ -0,0 +1,3 @@
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggleSidebar');
+toggleBtn.addEventListener('click', () => sidebar.classList.toggle('show'));
